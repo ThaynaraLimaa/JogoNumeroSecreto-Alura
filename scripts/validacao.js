998cba88fa29ae5dac6125d219a5f9d4137fa9dd
Thaynara <thaynaralimaa61@gmail.com>
2023-06-08 10:44:54 -0300

    Adicionando o comaando game over

--- scripts/validacao.js
+++ scripts/validacao.js
@@ -2,8 +2,16 @@ function verificaSeOValorEValido(chute) {
     const numero = +chute // converter para um n´mero inteiro
 
     if(chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>valor inválido</div>'
-        return
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML = `
+            <h2>Game Over</h2>
+            <h3>O número secreto era ${numeroSecreto}.</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+            `
+        } else {
+            elementoChute.innerHTML += '<div>valor inválido</div>'
+            return
+        }
     }
 
     if (numeroMaiorOuMenorQueValorPermitido(numero)) {
@@ -17,6 +25,7 @@ function verificaSeOValorEValido(chute) {
         <h3>O número secreto era ${numeroSecreto}</h3>
         <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
         `
+        recognition.stop()
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
             <div>O número é menor <i class="fa-solid fa-arrow-down"></i></div>
